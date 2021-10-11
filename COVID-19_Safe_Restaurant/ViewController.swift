215917705420169e887871441deb8d7529e4b0c5
kimjunsoo <rlawnstn01023@naver.com>
2021-10-11 16:20:32 +0900

    Initial Commit

---
+++ COVID-19_Safe_Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID-19_Safe_Restaurant
+//
+//  Created by 준수김 on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
